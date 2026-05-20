c073aa219c9a85981c3e9b8bb76f3ec178f186cf
RiteshSahany <SahanyRitesh@proton.me>
2026-05-20 19:30:21 +0530

    Settings: Add GenesisOS Maintainer Preference
    Change-Id: Ie3a0e420d6f6d0f0304da92069578039e850d2d4

--- src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
@@ -73,6 +73,7 @@ open class FirmwareVersionScreen : PreferenceScreenMixin, PreferenceSummaryProvi
             +GenesisVersionPreference()
             +SecurityPatchLevelPreference()
             +MainlineModuleVersionPreference()
+            +GenesisMaintainerPreference()
             +BasebandVersionPreference()
             +KernelVersionPreference()
             +SimpleBuildNumberPreference()
